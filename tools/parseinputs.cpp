40661079ff03f4e24130c9e69f82866fa038531b
Koray Yanik <fumyuun@gmail.com>
2016-08-18 22:20:39 +0200

    Write outputs as .coe file, useable by vivado's bram generator.

--- tools/parseinputs.cpp
+++ tools/parseinputs.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <iomanip>
 
 struct snes_js_t {
     bool up;
@@ -53,8 +54,23 @@ struct snes_js_t {
     };
 
     friend std::ostream& operator << (std::ostream& output, const snes_js_t& js) {
+        unsigned int out = 0x0000;
+
+        if (js.b) {     out |= (1 <<  0);}
+        if (js.y) {     out |= (1 <<  1);}
+        if (js.select) {out |= (1 <<  2);}
+        if (js.start) { out |= (1 <<  3);}
+        if (js.up) {    out |= (1 <<  4);}
+        if (js.down) {  out |= (1 <<  5);}
+        if (js.left) {  out |= (1 <<  6);}
+        if (js.right) { out |= (1 <<  7);}
+        if (js.a) {     out |= (1 <<  8);}
+        if (js.x) {     out |= (1 <<  9);}
+        if (js.l) {     out |= (1 << 10);}
+        if (js.r) {     out |= (1 << 11);}
+
         if (output.good()) {
-            output << js.up << js.down << js.left << js.right << js.start << js.select << js.a << js.b << js.x << js.y << js.l << js.r;
+            output << std::hex << std::setw(4) << std::setfill('0') << out;
         }
         return output;
     };
@@ -104,7 +120,25 @@ int main (int argc, char **argv) {
 
     if (input.bad()) {
         std::cout << "read error" << std::endl;
+        input.close();
+        output.close();
+        return 4;
     }
+
+    output << "memory_initialization_radix=16;" << std::endl;
+    output << "memory_initialization_vector=" << std::endl;
+
+    int linecount = 0;
+    std::list<snes_js_t>::iterator it;
+    for (it = snes_list.begin(); it != snes_list.end(); ++it) {
+        output << *it << " ";
+        if (linecount++ > 16) {
+            output << std::endl;
+            linecount = 0;
+        }
+    }
+
+    output << ";" << std::endl;
 
     std::cout << "Succesfully parsed " << snes_list.size() << " inputs!" << std::endl;
 }
